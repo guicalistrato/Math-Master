ddb3176d70dfef6989f491e6046ba4ae325d3fde
guicalistrato <gcnt100@gmail.com>
2026-04-22 22:24:48 -0300

    alterei as rotas para a forma mais moderna

--- app.py
+++ app.py
@@ -51,140 +51,139 @@ def index():
 
     return render_template("index.html", username=username)
 
-
-# Test
-@app.route("/test_home", methods=["GET", "POST"])
+# Home dos Tests
+@app.get("/test_home")
 @login_required
-def test_home():
+def test_home_get():
+    name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+    username = name[0]['username']
+    return render_template("test_home.html", username=username)
+
+@app.post("/test_home")
+@login_required
+def test_home_post():
     """Redirect to Test Home Page"""
     # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        # Get questions number
-        questions = get_questions()
+    # Get questions number
+    questions = get_questions()
 
-        # Generate test ID
-        test_id = generate_test_id(questions)
+    # Generate test ID
+    test_id = generate_test_id(questions)
 
-        # Get questions
-        for i in range(10):
-            a = db.execute("SELECT a FROM questions WHERE number = ?", questions[i])[0]["a"]
-            b = db.execute("SELECT b FROM questions WHERE number = ?", questions[i])[0]["b"]
-            c = db.execute("SELECT c FROM questions WHERE number = ?", questions[i])[0]["c"]
-            d = db.execute("SELECT d FROM questions WHERE number = ?", questions[i])[0]["d"]
-            question_text = db.execute("SELECT question FROM questions WHERE number = ?", questions[i])[
-                0]["question"]
-            answer = db.execute("SELECT answer FROM questions WHERE number = ?",
-                                questions[i])[0]["answer"]
-            difficulty = db.execute("SELECT difficulty FROM questions WHERE number = ?", questions[i])[
-                0]["difficulty"]
-            db.execute(
-                "INSERT INTO tests (test_id, question_number, question_text, a, b, c, d, answer, difficulty, time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", test_id, questions[
-                    i], question_text, a, b, c, d, answer, difficulty, datetime.now()
-            )
+    # Get questions
+    for i in range(10):
+        a = db.execute("SELECT a FROM questions WHERE number = ?", questions[i])[0]["a"]
+        b = db.execute("SELECT b FROM questions WHERE number = ?", questions[i])[0]["b"]
+        c = db.execute("SELECT c FROM questions WHERE number = ?", questions[i])[0]["c"]
+        d = db.execute("SELECT d FROM questions WHERE number = ?", questions[i])[0]["d"]
+        question_text = db.execute("SELECT question FROM questions WHERE number = ?", questions[i])[
+            0]["question"]
+        answer = db.execute("SELECT answer FROM questions WHERE number = ?",
+                            questions[i])[0]["answer"]
+        difficulty = db.execute("SELECT difficulty FROM questions WHERE number = ?", questions[i])[
+            0]["difficulty"]
+        db.execute(
+            "INSERT INTO tests (test_id, question_number, question_text, a, b, c, d, answer, difficulty, time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", test_id, questions[
+                i], question_text, a, b, c, d, answer, difficulty, datetime.now()
+        )
 
-        # Render the test
-        name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
-        username = name[0]['username']
-        test = db.execute("SELECT * FROM tests WHERE test_id = ?", test_id)
-        return render_template("test.html", test=test, username=username)
+    # Render the test
+    name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+    username = name[0]['username']
+    test = db.execute("SELECT * FROM tests WHERE test_id = ?", test_id)
+    return render_template("test.html", test=test, username=username)
 
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
-        username = name[0]['username']
-        return render_template("test_home.html", username=username)
-
-
-@app.route("/test", methods=["GET", "POST"])
+# testes em si
+@app.get("/test")
 @login_required
-def test():
+def test_get():
+    return render_template("/")
+
+@app.post("/test")
+@login_required
+def test_post():
     """Create a Test and show results"""
     # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        """Display the test"""
-        # Get Test ID
-        test_id = db.execute("SELECT test_id FROM tests ORDER BY time DESC LIMIT 1")[0]["test_id"]
+    """Display the test"""
+    # Get Test ID
+    test_id = db.execute("SELECT test_id FROM tests ORDER BY time DESC LIMIT 1")[0]["test_id"]
 
-        # List questions' subjects
-        sub = db.execute("SELECT subject FROM questions WHERE number IN (SELECT question_number FROM tests WHERE test_id = ?)",
-                         test_id)
-        subject = []
-        for i in range(10):
-            subject.append(sub[i]["subject"])
+    # List questions' subjects
+    sub = db.execute("SELECT subject FROM questions WHERE number IN (SELECT question_number FROM tests WHERE test_id = ?)",
+                        test_id)
+    subject = []
+    for i in range(10):
+        subject.append(sub[i]["subject"])
 
-        # Get Correct Answers
-        correct_answers = []
-        for i in range(10):
-            correct_answers.append(db.execute(
-                "SELECT answer FROM tests WHERE test_id = ?", test_id)[i]["answer"])
+    # Get Correct Answers
+    correct_answers = []
+    for i in range(10):
+        correct_answers.append(db.execute(
+            "SELECT answer FROM tests WHERE test_id = ?", test_id)[i]["answer"])
 
-        # Get User Answers (ChatGPT helped)
-        user_answers = [request.form.get(f"question_{i}") for i in range(1, 11)]
+    # Get User Answers (ChatGPT helped)
+    user_answers = [request.form.get(f"question_{i}") for i in range(1, 11)]
 
-        # Check
-        correctness = []
-        review = []
-        questions_correct = 0
-        for i in range(10):
-            if correct_answers[i] == user_answers[i]:
-                correctness.append("correct")
-                questions_correct += 1
-            else:
-                correctness.append("incorrect")
-                if sub[i]["subject"] not in review:
-                    review.append(sub[i]["subject"])
-
-        # Custom Message
-        if questions_correct < 5:
-            message = "Oops! Better luck next time!"
-        elif questions_correct in [5, 6]:
-            message = "You're almost there. Don't give up!"
-        elif questions_correct in [7, 8, 9]:
-            message = "Very nice! Your math skills are impressive"
+    # Check
+    correctness = []
+    review = []
+    questions_correct = 0
+    for i in range(10):
+        if correct_answers[i] == user_answers[i]:
+            correctness.append("correct")
+            questions_correct += 1
         else:
-            message = "Amazing! You did a fantastic job!"
+            correctness.append("incorrect")
+            if sub[i]["subject"] not in review:
+                review.append(sub[i]["subject"])
 
-        # Add to history
-        name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
-        user = name[0]['username']
-        time = db.execute("SELECT time FROM tests WHERE test_id = ?", test_id)
-        time = time[0]['time']
-
-        db.execute("INSERT INTO history (user, correct_answers, test_id, time) VALUES (?, ?, ?, ?)",
-                   user, questions_correct, test_id, time)
-
-        # Render Results
-        questions = [(i)for i in range(1, 11)]
-        return render_template("results.html", questions=questions, user_answers=user_answers, correct_answers=correct_answers, correctness=correctness, questions_correct=questions_correct, message=message, subject=subject, review=review)
-
+    # Custom Message
+    if questions_correct < 5:
+        message = "Oops! Better luck next time!"
+    elif questions_correct in [5, 6]:
+        message = "You're almost there. Don't give up!"
+    elif questions_correct in [7, 8, 9]:
+        message = "Very nice! Your math skills are impressive"
     else:
-        return render_template("/")
+        message = "Amazing! You did a fantastic job!"
 
+    # Add to history
+    name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+    user = name[0]['username']
+    time = db.execute("SELECT time FROM tests WHERE test_id = ?", test_id)
+    time = time[0]['time']
 
-@app.route("/forum", methods=["GET", "POST"])
+    db.execute("INSERT INTO history (user, correct_answers, test_id, time) VALUES (?, ?, ?, ?)",
+                user, questions_correct, test_id, time)
+
+    # Render Results
+    questions = [(i)for i in range(1, 11)]
+    return render_template("results.html", questions=questions, user_answers=user_answers, correct_answers=correct_answers, correctness=correctness, questions_correct=questions_correct, message=message, subject=subject, review=review)
+    
+# rota do forum
+@app.get("/forum")
 @login_required
-def forum():
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        # Get Post Informations
-        name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
-        user = name[0]['username']
-        post = request.form.get("post")
-        time = datetime.now()
+def forum_get():
+    # Get values from database
+    posts = db.execute("SELECT * FROM forum ORDER BY time DESC")
+    return render_template("forum.html", posts=posts)
 
-        # Insert Post into forum table
-        db.execute("INSERT INTO forum (user, post, time) VALUES (?, ?, ?)", user, post, time)
+@app.post("/forum")
+@login_required
+def forum_post():
+    # Get Post Informations
+    name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+    user = name[0]['username']
+    post = request.form.get("post")
+    time = datetime.now()
 
-        # Get values from database
-        posts = db.execute("SELECT * FROM forum")
+    # Insert Post into forum table
+    db.execute("INSERT INTO forum (user, post, time) VALUES (?, ?, ?)", user, post, time)
 
-        return render_template("forum.html", posts=posts)
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        # Get values from database
-        posts = db.execute("SELECT * FROM forum ORDER BY time DESC")
-        return render_template("forum.html", posts=posts)
+    # Get values from database
+    posts = db.execute("SELECT * FROM forum")
 
+    return render_template("forum.html", posts=posts)
 
 # History
 @app.route("/history")
@@ -198,13 +197,11 @@ def history():
     # Render index page
     return render_template("history.html", info=info)
 
-
 @app.route("/learn")
 @login_required
 def learn():
     """Redirect to Learn page"""
     return render_template("learn.html")
-
 
 @app.route("/functions")
 @login_required
@@ -212,13 +209,11 @@ def functions():
     """Redirect to Functions page"""
     return render_template("functions.html")
 
-
 @app.route("/geometry")
 @login_required
 def geometry():
     """Redirect to Geometry page"""
     return render_template("geometry.html")
-
 
 @app.route("/trigonometry")
 @login_required
@@ -226,13 +221,11 @@ def trigonometry():
     """Redirect to Trigonometry page"""
     return render_template("trigonometry.html")
 
-
 @app.route("/proportion")
 @login_required
 def proportion():
     """Redirect to Proportion page"""
     return render_template("proportion.html")
-
 
 @app.route("/financial_math")
 @login_required
@@ -240,98 +233,91 @@ def financial_math():
     """Redirect to Financial Math page"""
     return render_template("financial_math.html")
 
-
-# Routes (almost) equal to their finance correspondants
 # Login
-@app.route("/login", methods=["GET", "POST"])
-def login():
+@app.get("/login")
+def login_get():
+    session.clear()
+    return render_template("login.html")
+
+@app.post("/login")
+def login_post():
     """Log user in"""
 
     # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+    # Ensure username was submitted
+    if not request.form.get("username"):
+        return apology("must provide username", 403)
 
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
+    # Ensure password was submitted
+    elif not request.form.get("password"):
+        return apology("must provide password", 403)
 
-        # Query database for username
-        rows = db.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username")
-        )
+    # Query database for username
+    rows = db.execute(
+        "SELECT * FROM users WHERE username = ?", request.form.get("username")
+    )
 
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows[0]["hash"], request.form.get("password")
-        ):
-            return apology("invalid username and/or password", 403)
+    # Ensure username exists and password is correct
+    if len(rows) != 1 or not check_password_hash(
+        rows[0]["hash"], request.form.get("password")
+    ):
+        return apology("invalid username and/or password", 403)
 
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+    # Remember which user has logged in
+    session["user_id"] = rows[0]["id"]
 
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
+    # Redirect user to home page
+    return redirect("/")
 
 # Register
-@app.route("/register", methods=["GET", "POST"])
-def register():
+@app.get("/register")
+def register_get():
+    return render_template("register.html")
+
+@app.post("/register")
+def register_post():
     """Register user"""
 
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 400)
+    # Ensure username was submitted
+    if not request.form.get("username"):
+        return apology("must provide username", 400)
 
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 400)
+    # Ensure password was submitted
+    elif not request.form.get("password"):
+        return apology("must provide password", 400)
 
-        # Ensure confimation was submitted
-        elif not request.form.get("confirmation"):
-            return apology("must confirm password", 400)
+    # Ensure confimation was submitted
+    elif not request.form.get("confirmation"):
+        return apology("must confirm password", 400)
 
-        # Check if password and confirmation are equal
-        if request.form.get("password") != request.form.get("confirmation"):
-            return apology("passwords do not match", 400)
+    # Check if password and confirmation are equal
+    if request.form.get("password") != request.form.get("confirmation"):
+        return apology("passwords do not match", 400)
 
-        # Check if username is already taken
-        rows = db.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username")
-        )
-        if len(rows) != 0:
-            return apology("username already taken", 400)
+    # Check if username is already taken
+    rows = db.execute(
+        "SELECT * FROM users WHERE username = ?", request.form.get("username")
+    )
+    if len(rows) != 0:
+        return apology("username already taken", 400)
 
-        # Add user to the table
-        db.execute(
-            "INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get(
-                "username"), generate_password_hash(request.form.get("password"))
-        )
+    # Add user to the table
+    db.execute(
+        "INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get(
+            "username"), generate_password_hash(request.form.get("password"))
+    )
 
-        nrows = db.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username")
-        )
+    nrows = db.execute(
+        "SELECT * FROM users WHERE username = ?", request.form.get("username")
+    )
 
-        # Remember which user has logged in
-        session["user_id"] = nrows[0]["id"]
+    # Remember which user has logged in
+    session["user_id"] = nrows[0]["id"]
 
-        # Redirect user to home page
-        return redirect("/")
-
-        # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("register.html")
-
+    # Redirect user to home page
+    return redirect("/")
 
 # Logout
 @app.route("/logout")
